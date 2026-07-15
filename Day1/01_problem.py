3a115abdcf3d8480cb947c81cba669db1d857f8b
Abhinandan Kumar <abhi.mybusiness8@gmail.com>
2026-07-15 13:25:42 +0530

    Organized Python practice into day-wise folders

---
+++ Day1/01_problem.py
@@ -0,0 +1,11 @@
+# Take a string as user input and check whether it is a palindrome or not.
+
+
+a = input("Enter a string")
+
+if a == (a[::-1]):
+    print("This is a palindrome")
+
+else:
+    print("This is not a palindrome")    
+
---
+++ Day1/02_problem.py
@@ -0,0 +1,16 @@
+# Given the list [5, 2, 8, 1, 9, 3], find the largest and smallest number using a loop (without using max()/min()).
+
+
+
+num = [5, 2, 8, 1, 9, 3]
+biggest = num[0]
+smallest = num[0]
+
+for i in num:
+    if i > biggest:
+        biggest = i
+    if i < smallest:
+        smallest = i
+
+print("biggest", biggest)
+print("smallest", smallest)        
